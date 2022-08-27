5d2834fc28e647fa6142b006f534cd20b5057b39
EugenefedorovPro <eugene.proskulikov@gmail.com>
2022-08-27 18:21:41 +0300

    word2ipa main done

---
+++ word2ipa_rus/utils/word_processing.py
@@ -0,0 +1,51 @@
+import numpy as np
+
+
+class WordsProcessing:
+    @classmethod
+    def word2numbers(cls, accent):
+        character2number_for_stressed_words = {
+            "'": 1,
+            "-": 2,
+            "а": 3,
+            "б": 4,
+            "в": 5,
+            "г": 6,
+            "д": 7,
+            "е": 8,
+            "ж": 9,
+            "з": 10,
+            "и": 11,
+            "й": 12,
+            "к": 13,
+            "л": 14,
+            "м": 15,
+            "н": 16,
+            "о": 17,
+            "п": 18,
+            "р": 19,
+            "с": 20,
+            "т": 21,
+            "у": 22,
+            "ф": 23,
+            "х": 24,
+            "ц": 25,
+            "ч": 26,
+            "ш": 27,
+            "щ": 28,
+            "ъ": 29,
+            "ы": 30,
+            "ь": 31,
+            "э": 32,
+            "ю": 33,
+            "я": 34,
+            "ё": 35,
+        }
+        max_length_of_stressed_word = 34
+        numbers = []
+        for ch in accent:
+            n = character2number_for_stressed_words[ch]
+            numbers.append(n)
+        n_of_zeros_to_add = max_length_of_stressed_word - len(numbers)
+        numbers.extend([0 for i in range(n_of_zeros_to_add)])
+        return numbers
---
+++ word2ipa_rus/word2ipa.py
@@ -0,0 +1,39 @@
+from word2ipa_rus.utils.ipa_processing import IpaProcessing
+from word2ipa_rus.utils.word_processing import WordsProcessing
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+import numpy as np
+from pathlib import Path
+
+# import trained model
+path_model = Path(__file__).parent / "model_word2ipa_rus.h5"
+model_ipa = load_model(path_model)
+
+
+def make_array_from_word_for_model(word_stressed):
+    no_classes = 90
+    n_columns_labels = 34
+    word_as_array = WordsProcessing.word2numbers(word_stressed)
+    word_as_array = np.array(word_as_array)
+    word_as_array = word_as_array.reshape(1, word_as_array.shape[0])
+    predicted_array = model_ipa.predict(word_as_array, verbose=0)
+    predicted_array = predicted_array.reshape(n_columns_labels, no_classes)
+    return predicted_array
+
+
+def turn_predicted_array_to_transcription(predicted_array):
+    number2sign = IpaProcessing.get_number2sign()
+    ipa_predicted = []
+    for position in predicted_array:
+        number = np.argmax(position)
+        if number != 0:
+            sign = number2sign[number]
+            ipa_predicted.append(str(sign))
+    ipa_predicted = "".join(ipa_predicted)
+    return ipa_predicted
+
+
+def word2ipa(word_stressed):
+    predicted_array = make_array_from_word_for_model(word_stressed)
+    ipa_predicted = turn_predicted_array_to_transcription(predicted_array)
+    return ipa_predicted
